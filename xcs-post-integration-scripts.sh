567bb76710768f6c7b60c4097059ea73b111b658
traintrackcn <traintrackcn@gmail.com>
2018-04-27 00:16:33 +0800

    * removed unnecessary params in scripts

--- xcs-post-integration-scripts.sh
+++ xcs-post-integration-scripts.sh
@@ -1,8 +1,6 @@
 API_SERVER="https://traintrackcn.asuscomm.com"
 UUID="ED8CE088-F013-4C4E-A9CE-1507905DFEB0"
 APP_ID="com.abovegem.app.ios.XCSTester"
-APP_REPO="${XCS_PRIMARY_REPO_DIR}"
-APP_FILE="${XCS_PRODUCT}"
 ICON_FILE="${XCS_PRIMARY_REPO_DIR}/XCSTester/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@3x.png"
 TRACKER_TOKEN="123213"
 
@@ -10,8 +8,6 @@ bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
 -H "x-app-icon: ${ICON_FILE}" \
--H "x-app-repo: ${APP_REPO}" \
--H "x-app-file: ${APP_FILE}" \
 -H "x-device-uuid: ${UUID}"  \
 -s "${API_SERVER}/0/apps/${APP_ID}/xcs-post-integration-scripts" \
 &)
--- xcs-pre-integration-scripts.sh
+++ xcs-pre-integration-scripts.sh
@@ -1,13 +1,11 @@
 API_SERVER="https://traintrackcn.asuscomm.com"
 UUID="ED8CE088-F013-4C4E-A9CE-1507905DFEB0"
 APP_ID="com.abovegem.app.ios.XCSTester"
-APP_REPO="${XCS_PRIMARY_REPO_DIR}"
 
 bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
 -H "x-device-uuid: ${UUID}"  \
--H "x-app-repo: ${APP_REPO}" \
 -s "${API_SERVER}/0/apps/${APP_ID}/xcs-pre-integration-scripts" \
 &)
 wait
