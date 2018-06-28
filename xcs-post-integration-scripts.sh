0ed1b2384cc18604958334608d8e63e9dac92531
Tao Yunfei <traintrackcn@gmail.com>
2018-06-28 20:10:59 +0800

    ...

--- xcs-post-integration-scripts.sh
+++ xcs-post-integration-scripts.sh
@@ -1,12 +1,10 @@
 API_SERVER="http://localhost:8080"
-UUID="ED8CE088-F013-4C4E-A9CE-1507905DFEB0"
 AUTH_ID="n8-app-ios-dev"
 TRACKER_TOKEN="123213"
 
 bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
--H "x-device-uuid: ${UUID}"  \
 -s "${API_SERVER}/0/apps/${AUTH_ID}/xcs-post-integration-scripts" \
 &)
 wait
--- xcs-pre-integration-scripts.sh
+++ xcs-pre-integration-scripts.sh
@@ -1,11 +1,9 @@
 API_SERVER="http://localhost:8080"
-UUID="ED8CE088-F013-4C4E-A9CE-1507905DFEB0"
 AUTH_ID="n8-app-ios-dev"
 
 bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
--H "x-device-uuid: ${UUID}"  \
 -s "${API_SERVER}/0/apps/${AUTH_ID}/xcs-pre-integration-scripts" \
 &)
 wait
