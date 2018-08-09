b80995a54e34da7a424ad68f898afe144624b765
Tao Yunfei <traintrackcn@gmail.com>
2018-08-09 23:39:58 +0800

    * updated prj setting

--- xcs-post.sh
+++ xcs-post.sh
@@ -1,10 +1,10 @@
 API_SERVER="http://litmonitor.asuscomm.com:8888"
-AUTH_ID="dev-monitor-playground-app-ios"
+PRJ_KEY_FOR_BUILD="NzlAaHR0cDovL3RyYWNrZXIuYWJvdmVnZW0uY29tLWFwcC1pb3M="
 TRACKER_TOKEN="123213"
 
 bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
--s "${API_SERVER}/0/apps/${AUTH_ID}/xcs-post-integration-scripts" \
+-s "${API_SERVER}/0/apps/${PRJ_KEY_FOR_BUILD}/xcs-post-integration-scripts" \
 &)
 wait
--- xcs-pre.sh
+++ xcs-pre.sh
@@ -1,9 +1,9 @@
 API_SERVER="http://litmonitor.asuscomm.com:8888"
-AUTH_ID="dev-monitor-playground-app-ios"
+PRJ_KEY_FOR_BUILD="NzlAaHR0cDovL3RyYWNrZXIuYWJvdmVnZW0uY29tLWFwcC1pb3M="
 
 bash <(
 curl \
 -H "x-api-server: ${API_SERVER}" \
--s "${API_SERVER}/0/apps/${AUTH_ID}/xcs-pre-integration-scripts" \
+-s "${API_SERVER}/0/apps/${PRJ_KEY_FOR_BUILD}/xcs-pre-integration-scripts" \
 &)
 wait
